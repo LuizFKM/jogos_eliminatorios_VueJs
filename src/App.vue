2ade22fdea7361e642c79186751362ad21e5937a
Luiz Francisco <luizfk.gmail.com>
2024-07-02 17:06:55 -0300

    configurando logica de tabelas e lista

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,97 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+const estado = reactive({
+  cadastraTime: '',
+  timesCadastrados: [],
+  listaDeClubes: false,
+  tabelaDeJogos: false,
+});
+
+const cadastrarClube = () => {
+  const clube = estado.cadastraTime;
+  if (clube.length > 3) {
+    alert('Clube cadastrado com sucesso!');
+    estado.timesCadastrados.push(clube);
+    estado.cadastraTime = '';
+  } else {
+    alert('Nome do clube inválido');
+  }
+};
+
+const listaClubes = () => {
+  estado.listaDeClubes = true;
+};
+
+const montaTabelaDeJogos = () => {
+  const tam = estado.timesCadastrados.length;
+  if (tam === 0 || tam % 2 === 1) {
+    alert("Deve haver número par de clubes");
+    return [];
+  }
+
+  let jogos = [];
+  const ultimo = estado.timesCadastrados.length - 1;
+
+  for (let i = 0; i < tam / 2; i++) {
+    const jogo = {
+      time1: estado.timesCadastrados[i],
+      time2: estado.timesCadastrados[ultimo - i]
+    };
+    jogos.push(jogo);
+  }
+  estado.tabelaDeJogos = true;
+  return jogos;
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header class="p-5 mt-5 text-center border-2 border-bottom border-success">
+    <h1>Jogos Eliminatórios</h1>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="row">
+    <div class="col-6 p-5 mt-5">
+      <form @submit.prevent="">
+        <div class="input-group mb-3">
+          <input 
+            type="text" 
+            class="form-control" 
+            placeholder="Digite o nome do clube" 
+            v-model="estado.cadastraTime"
+          >
+          <button type="button" class="btn btn-secondary" @click="cadastrarClube">Cadastrar</button>
+        </div>
+        <button type="button" id="btn-lista" class="me-2 btn btn-outline-secondary" @click="listaClubes">Listar clubes</button>
+        <button type="button" class="btn btn-outline-secondary" @click="montaTabelaDeJogos">Montar tabela de jogos</button>
+      </form>
+    </div>
+    <div v-if="estado.listaDeClubes" class="col-6 p-5 mt-5">
+      <ul class="list-group mt-4">
+        <li class="list-group-item" v-for="clube in estado.timesCadastrados" :key="clube">
+          {{ clube }}
+        </li>
+      </ul>
+    </div>
+    <div v-if="estado.tabelaDeJogos" class="col-6 p-5 mt-5">
+      <table class="table">
+        <thead>
+          <tr class="text-center">
+            <th scope="col">Time 1</th>
+            <th scope="col">X</th>
+            <th scope="col">Time 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(jogo, index) in montaTabelaDeJogos()" :key="index">
+            <td class="text-center">{{ jogo.time1 }}</td>
+            <td class="text-center">X</td>
+            <td class="text-center">{{ jogo.time2 }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
